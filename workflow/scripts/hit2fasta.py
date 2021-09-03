cbb5914c002b8f9d78f5669c654a28c45e124e1e
rdenise <remi.denise@gmail.com>
2021-09-03 13:50:10 +0200

    remove loop in make fasta

--- workflow/scripts/hit2fasta.py
+++ workflow/scripts/hit2fasta.py
@@ -7,17 +7,14 @@ sys.stderr = sys.stdout = open(snakemake.log[0], "w")
 # Get all the proteins of the database inside a dict-like structure
 all_index_fasta = SeqIO.index(snakemake.input.protein_fasta, 'fasta')
 
-# Read line by line the protein ftable hit without loading it in memory
+# Read line by line the protein table hit without loading it in memory
 with open(snakemake.input.list_all_prot, 'rt') as r_file :
-    header = r_file.readline().rstrip().split() 
-    index_proteinId = header.index('protein_id') 
-    proteins_of_interest = [] 
+    with open(snakemake.output.fasta, 'wt') as w_file :
+        header = r_file.readline().rstrip().split() 
+        index_proteinId = header.index('protein_id') 
 
-    for line in r_file : 
-        tmp_line = line.split() 
-        proteins_of_interest.append(tmp_line[index_proteinId]) 
-
-# Filtering protein table and saving
-with open(snakemake.output.fasta, 'w') as w_file :
-    for protein in proteins_of_interest :
-        SeqIO.write(all_index_fasta[protein], w_file, 'fasta')
+        for line in r_file : 
+            tmp_line = line.split() 
+            proteins_of_interest = tmp_line[index_proteinId]
+            SeqIO.write(all_index_fasta[proteins_of_interest], w_file, 'fasta')
+        
