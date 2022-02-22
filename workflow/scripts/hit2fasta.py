c23d737f25f836cf0aaf12abf84503124c7c6b4f
Remi Denise <remi.denise@gmail.com>
2022-02-22 17:48:50 +0100

    :bug: sequence name too long for psiblast

--- workflow/scripts/hit2fasta.py
+++ workflow/scripts/hit2fasta.py
@@ -7,6 +7,9 @@ sys.stderr = sys.stdout = open(snakemake.log[0], "w")
 # Get all the proteins of the database inside a dict-like structure
 all_index_fasta = SeqIO.index(snakemake.input.protein_fasta, 'fasta')
 
+# List of possible truncatenated ids
+list_cutname = []
+
 # Read line by line the protein table hit without loading it in memory
 with open(snakemake.input.list_all_prot, 'rt') as r_file :
     with open(snakemake.output.fasta, 'wt') as w_file :
@@ -16,5 +19,16 @@ with open(snakemake.input.list_all_prot, 'rt') as r_file :
         for line in r_file : 
             tmp_line = line.split() 
             proteins_of_interest = tmp_line[index_proteinId]
-            SeqIO.write(all_index_fasta[proteins_of_interest], w_file, 'fasta')
-        
+            
+            if proteins_of_interest in all_index_fasta:
+                SeqIO.write(all_index_fasta[proteins_of_interest], w_file, 'fasta')
+            else :
+                list_cutname.append(proteins_of_interest)
+
+        # Now parse the file to get the trucatenate fasta sequences
+        parser = SeqIO.index(snakemake.input.protein_fasta, 'fasta')
+
+        for prot in parser:
+            for cutname in list_cutname:
+                if prot.id.startswith(cutname):
+                    SeqIO.write(prot, w_file, 'fasta')
